34e74166915941ca8753c67adf7ea78a05c75c7f
mishi <mishi.beraia@gmail.com>
2023-07-29 17:37:26 +0400

    Implement the Add button

--- functions.py
+++ functions.py
@@ -1,7 +1,8 @@
 # Basically backend
+FILEPATH = "todos.txt"
 
 
-def get_todos(filepath='todos.txt'):
+def get_todos(filepath=FILEPATH):
     """ Opens file todos.txt, reads it,
     returns contents of the file
     """
@@ -10,7 +11,7 @@ def get_todos(filepath='todos.txt'):
     return todos_local
 
 
-def write_todos(todos_local, filepath='todos.txt'):
+def write_todos(todos_local, filepath=FILEPATH):
     """ Opens file, writes todos_local into it"""
     with open(filepath, 'w') as file_local:
         file_local.writelines(todos_local)
